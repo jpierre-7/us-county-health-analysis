196fcecd698e29295b70d0e95b144c1b0777ef3e
John Pierre <johndotpierre@outlook.com>
2026-06-02 15:44:06 -0500

    Started work on dashboard w/ dash and plotly. Baseplate

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,1 +1,10 @@
-# init commit
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("US County Health Analysis")
+])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8050)
